23161f31dcb139fe1f7b03079861780fcff1d9b8
Kira <user@userdeMacBook-Pro.local>
2014-07-11 11:22:10 +0800

    demo 2

--- Homework4/js/coverflow.js
+++ Homework4/js/coverflow.js
@@ -1,79 +1,132 @@
 var midimg=0;
-var Ecoverflow=document.getElementById('coverflow');
-var step=280;
-var visiblenumber=7;
+var gap=260;
+var visiblenumber=9;
 var unitopacity=1.0/visiblenumber;
-var animateDuration=500;
-var goleft=function (time){
-	console.log(midimg);
-	$(Ecoverflow).animate({left:'-='+step+'px'},time);
+var animateDuration=300;
+var goleft=function (Eimg,id,initialmid,finalmid,time){
+	console.log(id+' '+initialmid+' '+finalmid+' '+time);
+	$(Eimg).css('z-index',9999-Math.abs(finalmid-id));
+	$(Eimg).animate({'opacity':1-Math.abs(finalmid-id)*unitopacity},animateDuration);
+
+	if (id<finalmid) {
+		$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, -0.2, 0, 1, 0, 0) scale(1)'});
+	}
+	else if(id==finalmid){
+		$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0, 0, 1, 0, 0) scale(1.5)'});
+	}else{
+		$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0.2, 0, 1, 0, 0) scale(1)'});
+	}
+
+	if(id>initialmid&&id<finalmid){
+		$(Eimg).animate({left:'-='+2*gap+'px'},time);
+	}
+	if (id==initialmid||id==finalmid){
+		$(Eimg).animate({left:'-='+gap+'px'},time);
+	}
+	/*
+	for (j=initialmid;j<finalmid;j++){
+		if (id<j) {
+			$(Eimg).animate({opacity:'-='+unitopacity},time);
+		}else if(id==j){
+			$(Eimg).animate({opacity:'-='+unitopacity,left:'-='+gap+'px'},time);
+			$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, -0.5, 0, 1, 0, 0) scale(1)'});
+		}else if(id==j+1){
+			$(Eimg).animate({opacity:'1',left:'-='+gap+'px','z-index':'9999',},time);
+			$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0, 0, 1, 0, 0) scale(1.5)'});
+		}else{
+			$(Eimg).animate({opacity:'+='+unitopacity},time);
+		}
+	}
+	/*
 	for(i=0;i<N;i++){
 		$(Eimg[i]).css('z-index',9999-Math.abs(midimg+1-i));
 		if (i<midimg){
 			$(Eimg[i]).animate({opacity:'-='+unitopacity},time);
 		}else if(i==midimg){
-			$(Eimg[i]).animate({opacity:'-='+unitopacity,left:'-='+step+'px'},time);
+			$(Eimg[i]).animate({opacity:'-='+unitopacity,left:'-='+gap+'px'},time);
 			$(Eimg[i]).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, -0.5, 0, 1, 0, 0) scale(1)'});
 		}else if(i==midimg+1){
-			$(Eimg[i]).animate({opacity:'1',left:'-='+step+'px','z-index':'9999',},time);
+			$(Eimg[i]).animate({opacity:'1',left:'-='+gap+'px','z-index':'9999',},time);
 			$(Eimg[i]).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0, 0, 1, 0, 0) scale(1.5)'});
 		}else {
-			$(Eimg[i]).animate({opacity:'+='+unitopacity,'z-index':'+=1'},time);
+			$(Eimg[i]).animate({opacity:'+='+unitopacity},time);
 		}
-	}
+	}*/
 }
 
 
-var goright=function (time){
+var goright=function (Eimg,id,initialmid,finalmid,time){
+	$(Eimg).css('z-index',9999-Math.abs(finalmid-id));
+	$(Eimg).animate({'opacity':1-Math.abs(finalmid-id)*unitopacity},animateDuration);
+	if (id<finalmid) {
+		$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, -0.2, 0, 1, 0, 0) scale(1)'});
+	}
+	else if(id==finalmid){
+		$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0, 0, 1, 0, 0) scale(1.5)'});
+	}else{
+		$(Eimg).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0.2, 0, 1, 0, 0) scale(1)'});
+	}
+
+	if(id<initialmid&&id>finalmid){
+		$(Eimg).animate({left:'+='+2*gap+'px'},time);
+	}
+	if (id==initialmid||id==finalmid){
+		$(Eimg).animate({left:'+='+gap+'px'},time);
+	}
+	/*
 	console.log(midimg);
-	$(Ecoverflow).animate({left:'+='+step+'px'},time);
+	$(Ecoverflow).animate({left:'+='+gap+'px'},time);
 	for(i=0;i<N;i++){
 		$(Eimg[i]).css('z-index',9999-Math.abs(midimg-1-i));
 		if (i>midimg){
 			$(Eimg[i]).animate({opacity:'-='+unitopacity},time);
 		}else if(i==midimg){
-			$(Eimg[i]).animate({opacity:'-='+unitopacity,left:'+='+step+'px'},time);
+			$(Eimg[i]).animate({opacity:'-='+unitopacity,left:'+='+gap+'px'},time);
 			$(Eimg[i]).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0.5, 0, 1, 0, 0) scale(1)'});
 		}else if(i==midimg-1){
-			$(Eimg[i]).animate({opacity:'1',left:'+='+step+'px'},time);
+			$(Eimg[i]).animate({opacity:'1',left:'+='+gap+'px'},time);
 			$(Eimg[i]).css({'-webkit-transition':'all '+time+'ms','-webkit-transform':'matrix(1, 0, 0, 1, 0, 0) scale(1.5)'});
 		}else {
 			$(Eimg[i]).animate({opacity:'+='+unitopacity},time);
 		}
-	}
+	}*/
 }
 
 var move=function(){
-	var id=this.id.substr(14);
+	console.log(Ecoverflow);
+	var id=parseInt(this.id.substr(14));
 	console.log(id);
 	if (id>midimg){
 		var d=animateDuration/(id-midimg);
-		while (midimg<id) {
-			goleft(d);
-			midimg++;
-		}
+		$(Ecoverflow).animate({left:'-='+50*(id-midimg)+'px'},animateDuration);
+		for(i=0;i<N;i++) goleft(Eimg[i],i,midimg,id,d);
+		midimg=id;
 	}else if(id<midimg){
 		var d=animateDuration/(midimg-id);
-		while (midimg>id) {
-			goright(d);
-			midimg--;
-		}
+		$(Ecoverflow).animate({left:'+='+50*(midimg-id)+'px'},animateDuration);
+		for(i=0;i<N;i++) goright(Eimg[i],i,midimg,id,d);
+		midimg=id;
 	}
 }
 
 function docoverflow(){
 	Eimg=$('#coverflow img');
 	N=Eimg.length;
-	console.log(Eimg);
+	Ecoverflow=document.getElementById('coverflow');
+	//console.log(Eimg);
+	midimg=0;
 	for(i=0;i<N;i++){
-		if(i)$(Eimg[i]).css('left',-step*(i-1)+'px');
+		if(i)$(Eimg[i]).css('left',-gap*(i-1)+'px');
 		else $(Eimg[i]).css('left','0px');
 		$(Eimg[i]).bind({
 			click:move,
 		});
-		if(i)$(Eimg[i]).css({'-webkit-transform':'matrix(1, 0.5, 0, 1, 0, 0) scale(1)'});
-		else $(Eimg[i]).css({'-webkit-transform':'matrix(1, 0, 0, 1, 0, 0) scale(1.5)'});
-		$(Eimg[i]).css('z-index',9999-i);
+		if(i>midimg)$(Eimg[i]).css({'-webkit-transform':'matrix(1, 0.2, 0, 1, 0, 0) scale(1)'});
+		else if(i==midimg)$(Eimg[i]).css({'-webkit-transform':'matrix(1, 0, 0, 1, 0, 0) scale(1.5)'});
+		else $(Eimg[i]).css({'-webkit-transform':'matrix(1, -0.2, 0, 1, 0, 0) scale(1)'});
+
+		$(Eimg[i]).css({'z-index':9999-i,'opacity':1-i*unitopacity});
+		//console.log($(Eimg[i]).css('opacity'));
 		//console.log(Eimg[i].onclick);
 	}
 }
